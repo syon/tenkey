ed4675b853963760f9f15669741d9b36ef97a8db
syon <syon.xiii@gmail.com>
2019-06-16 21:01:52 +0900

    :seedling: First commit

---
+++ Gerbers/rename-elecrow.sh
@@ -0,0 +1,10 @@
+mkdir elecrow
+mv tenkey-F_Cu.gbr elecrow/tenkey.GTL
+mv tenkey-B_Cu.gbr elecrow/tenkey.GBL
+mv tenkey-F_Mask.gbr elecrow/tenkey.GTS
+mv tenkey-B_Mask.gbr elecrow/tenkey.GBS
+mv tenkey-F_SilkS.gbr elecrow/tenkey.GTO
+mv tenkey-B_SilkS.gbr elecrow/tenkey.GBO
+mv tenkey-PTH.drl elecrow/tenkey.TXT
+mv tenkey-NPTH.drl elecrow/tenkey-NPTH.TXT
+mv tenkey-Edge_Cuts.gbr elecrow/tenkey.GML
